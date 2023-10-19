8a04282e8d7ef77e300e9c5f36dacf56f9cf9ac5
amberlynn <amberlynn364@gmail.com>
2023-10-19 18:38:21 +0300

    feat: implement header, main, footer

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,12 @@
+import { Button } from '@mui/material';
+import './Header.scss';
+
+export default function Header() {
+  return (
+    <header className="header">
+      <div className="header-wrapper">
+        <Button>Kek</Button>
+      </div>
+    </header>
+  );
+}
