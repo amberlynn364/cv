af15b4d04a899c89ef509179c51b7bdec61f551c
amberlynn <amberlynn364@gmail.com>
2023-10-19 23:59:59 +0300

    feat: add button to More details

--- src/components/SkillsSection/SkillsSection.tsx
+++ src/components/SkillsSection/SkillsSection.tsx
@@ -1,5 +1,8 @@
+import { Button } from '@mui/material';
+import { Link } from 'react-router-dom';
 import SkillsTabs from '../SkillsTabs/SkillsTabs';
 import './SkillsSection.scss';
+import RouterPaths from '../../router/router-types';
 
 export default function SkillsSection() {
   return (
@@ -7,6 +10,14 @@ export default function SkillsSection() {
       <div className="skills-wrapper">
         <p className="subtitle">Why hire me</p>
         <SkillsTabs />
+        <Button
+          variant="contained"
+          component={Link}
+          to={RouterPaths.AboutMe}
+          sx={{ width: 'max-content', marginTop: '30px' }}
+        >
+          More Details
+        </Button>
       </div>
     </section>
   );
