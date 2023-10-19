b8ae87b16e26cee3a4e692b67592d69c12d42296
amberlynn <amberlynn364@gmail.com>
2023-10-19 23:39:27 +0300

    feat: add navigation to header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,16 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import { Button } from '@mui/material';
 import './Header.scss';
+import Navigation from '../Navigation/Navigation';
 
 export default function Header() {
   return (
     <header className="header">
       <div className="header-wrapper">
-        <Button>Kek</Button>
+        <Button />
+        <nav className="header-nav">
+          <Navigation />
+        </nav>
       </div>
     </header>
   );
