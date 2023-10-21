4f1cf2b421fd741e7648ae6d394b6801b80c951c
amberlynn <amberlynn364@gmail.com>
2023-10-21 15:56:58 +0300

    feat: update Footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,5 +1,13 @@
+import ContactLinks from './ContactLinks/ContactLinks';
 import './Footer.scss';
 
 export default function Footer() {
-  return <footer className="footer">Footer</footer>;
+  const currentYear = new Date().getFullYear();
+
+  return (
+    <footer id="contacts" className="footer">
+      <p className="footer__copyright">{`© ${currentYear} by Ivan Timoshkin`}</p>
+      <ContactLinks />
+    </footer>
+  );
 }
---
+++ src/data/contacts.ts
@@ -0,0 +1,21 @@
+interface IContacts {
+  title: string;
+  link: string;
+}
+
+const contacts: IContacts[] = [
+  {
+    title: 'Github',
+    link: 'https://github.com/amberlynn364',
+  },
+  {
+    title: 'Telegram',
+    link: 'https://t.me/amberlynn364',
+  },
+  {
+    title: 'Email',
+    link: 'mailto:amberlynn364@gmail.com',
+  },
+];
+
+export default contacts;
